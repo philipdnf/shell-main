1d061c0eacc2e6d9abc0c3565a6adad26655823b
philipdnf <philipdnf@gmail.com>
2021-03-23 14:25:57 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'news',    
+    loadChildren: () =>
+      loadRemoteModule({        
+        remoteName: 'mf_news',
+        exposedModule: './Module'
+      })
+      .then(m => m.NewsModule)
+  },  
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
